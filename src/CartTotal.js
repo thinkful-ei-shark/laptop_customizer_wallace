0ec8148438d3a75b3d6a100915f3085ef4536e94
twallace1015@gmail.com <68206140+TomWallaceJr@users.noreply.github.com>
2020-10-04 12:56:23 -0400

    carttotal completed

--- src/CartTotal.js
+++ src/CartTotal.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import USCurrencyFormat from './USCurrencyFormat';
 
-function Cart(props){
-    return (            
+function Cart(props) {
+    return (
         <div className="summary__total__value">
             {USCurrencyFormat.format(props.total)}
         </div>
     )
 }
+
 
 export default Cart;
 
